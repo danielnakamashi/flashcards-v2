f79bf7844ffd776049687acf570064079f5ae753
Daniel Nakamashi <daniel.nakamashi@auto1.com>
2019-10-28 22:47:09 +0100

    Styled header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { UserContext } from '../../containers/Authentication';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    userName: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 const Header: React.FC = () => {
   const { user, signOut } = React.useContext(UserContext);
+  const styles = useStyles();
 
   return (
-    <header>
-      {user && (
-        <div>
-          <img src={user.photoURL || undefined} alt={user.displayName || undefined} />
-          <span>{user.displayName}</span>
-        </div>
-      )}
-      <button onClick={signOut}>Logout</button>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        {user && (
+          <>
+            <IconButton>
+              <Avatar src={user.photoURL || undefined} alt={user.displayName || undefined}></Avatar>
+            </IconButton>
+            <Typography className={styles.userName}>{user.displayName}</Typography>
+            <Button variant="contained" onClick={signOut}>
+              Logout
+            </Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
