9745446a738c8c1421d7030463cebfe1cc3e8097
Daniel Nakamashi <dnakamashi@gmail.com>
2020-09-16 14:54:08 +0200

    Coverage (#32)
    * chore: add test to remove topic
    
    * chore: add more tests and refactor

--- packages/web/src/pages/TopicPage/TopicPage.test.tsx
+++ packages/web/src/pages/TopicPage/TopicPage.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { TopicRepositoryMemory } from '@flashcards/service';
 import TopicPage from './TopicPage';
 import { AppProvider } from '../../contexts/AppContext';
-import { TopicRepositoryMemory } from '../../mocks/TopicRepositoryMemory';
 
 describe('<TopicPage />', () => {
   it('should render', () => {
@@ -24,5 +24,29 @@ describe('<TopicPage />', () => {
     );
 
     expect(getByTestId('topic-page')).toBeInTheDocument();
+  });
+
+  it('should add card', async () => {
+    const { getByLabelText, getByText, findByText } = render(
+      <AppProvider value={{ topicRepository: new TopicRepositoryMemory() }}>
+        <MemoryRouter initialEntries={['/1']}>
+          <TopicPage
+            user={{
+              displayName: 'User Name',
+              email: 'email@example.com',
+              photoURL: 'https://via.placeholder.com/150',
+              uid: '123',
+            }}
+            logout={jest.fn()}
+          />
+        </MemoryRouter>
+      </AppProvider>,
+    );
+
+    fireEvent.change(getByLabelText(/Question/), { target: { value: 'Question 1' } });
+    fireEvent.change(getByLabelText(/Answer/), { target: { value: 'Answer 1' } });
+    fireEvent.click(getByText('Add Card'));
+
+    expect(await findByText('Question 1')).toBeInTheDocument();
   });
 });
