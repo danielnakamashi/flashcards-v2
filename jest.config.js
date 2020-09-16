e89389b4d32b404d6bd52c090b594f6d2bafa523
Daniel Nakamashi <dnakamashi@gmail.com>
2020-09-16 17:52:33 +0200

    chore: increase jest timeout

--- jest.config.js
+++ jest.config.js
@@ -141,7 +141,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './jest.setup.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
