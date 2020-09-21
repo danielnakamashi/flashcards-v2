66cabad99231936be1af36491433ffc3365a1dcd
Daniel Nakamashi <dnakamashi@gmail.com>
2020-09-21 13:05:59 +0200

    Lint (#41)
    * feat: remove card
    
    * chore: remove TopicRepositoryLocalStorage
    
    * chore: remove unused files
    
    * chore: fix test
    
    * chore: fix test
    
    * chore: fix lint
    
    * chore: fix yarn:lint script
    
    * chore: fix pre-commit command

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react'],
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
